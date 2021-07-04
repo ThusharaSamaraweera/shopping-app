13972b00a0eccbdeb1962dcc039bcb7f1b1a4b33
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-07-04 16:48:48 +0530

    completed banner

--- src/components/ShoppingArea.tsx
+++ src/components/ShoppingArea.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ShoppingAreaNavBar from "./BottomNavBar";
+import Banner from "./Banner";
 
 const ShoppingArea: React.FC = () => {
   return (
     <div className='shopping-area'>
       <ShoppingAreaNavBar/>
-
+      <Banner/>
     </div>
   );
 }
