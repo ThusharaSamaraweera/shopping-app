324b0f2be258a82a186e9458f00ebd909ca46aca
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-07-04 11:30:03 +0530

    completed bottom nav bar

---
+++ src/components/BottomNavBar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {Navbar, Row, Nav, NavDropdown, Container} from "react-bootstrap";
+
+const BottomNabBar: React.FC = () => {
+  return (
+    <Container>
+      <Row className='shopping-area-nav-bar'>
+        <Navbar expand="sm" className='nav-nar-third'>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavDropdown className='nav-drop-down' title="Categories" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">All</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Grocery</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Pharmacy</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Food</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Electronic</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">FAQ</Nav.Link>
+              <Nav.Link href="#About">About Us</Nav.Link>
+              <Nav.Link href="#Contact">Contact Us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Row>
+    </Container>
+  );
+}
+
+export default BottomNabBar;
--- src/views/ShoppingApp.tsx
+++ src/views/ShoppingApp.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import {Container} from "react-bootstrap";
 import TopNavBar from "../components/TopNavBar";
 import MiddleNavBar from "../components/MiddleNavBar";
+import ShoppingArea from "../components/ShoppingArea";
 
 const ShoppingApp: React.FC = () => {
   return (
     <Container fluid={true}>
       <TopNavBar/>
       <MiddleNavBar/>
+      <ShoppingArea/>
     </Container>
   );
 }
