28690f0bf56c96926bd052767d3417aa90e19146
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-11-29 07:05:59 +0530

    initiaze account page

--- src/views/ShoppingApp.tsx
+++ src/views/ShoppingApp.tsx
@@ -8,8 +8,6 @@ import Footer from "../components/Footer";
 const ShoppingApp: React.FC = () => {
   return (
     <Container fluid={true}>
-      <TopNavBar/>
-      <MiddleNavBar/>
       <ShoppingArea/>
       <Footer/>
     </Container>
