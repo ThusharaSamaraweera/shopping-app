29483fe4035224358079446bfcd0aa22e2d9ce95
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-03-14 14:42:22 +0530

    add shipping and billing address title

--- src/components/checkoutShippingForm/index.tsx
+++ src/components/checkoutShippingForm/index.tsx
@@ -8,6 +8,13 @@ const index = () => {
       <Row className='shopping-form-area'>
         <Col className='shopping-form-area-div' xs={12} sm={12} md={6} lg={{span: 7, offset: 5}}>
           <SigninArea />
+
+          <Row className='billing-address'>
+            <Col className='billing-address-header'>
+             <h5>Shipping and Billing Address</h5>
+
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
