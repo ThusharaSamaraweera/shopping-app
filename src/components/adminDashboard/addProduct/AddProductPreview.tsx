530ade32fd52116ca49d487e1a40550186c5ce4b
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-06-05 17:30:26 +0530

    add product preview card to add product page

---
+++ src/components/adminDashboard/addProduct/AddProductPreview.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import {Col, Row, Image, Button} from "react-bootstrap";
+import NumberFormat from "react-number-format";
+
+type AddProductPreviewProps = {
+  productName: string
+  price: number | null
+  discount: number | null
+  productImage: null | string
+}
+
+const AddProductPreview: React.FC<AddProductPreviewProps> = (props) => {
+  const image: any = require(`../../../asserts/images/insertImage.webp`);
+  const {productName, price, discount} = props;
+
+  const priceMain = (price: number): number | string => {
+    if (!price) {
+      return "";
+    }
+    return Math.trunc(price);
+  }
+
+  const priceCents = (price: number) => {
+    if (price % 1 === 0) {
+      return '00';
+    } else {
+      return Math.trunc(price % 1 * 100);
+    }
+  }
+  const renderImage = () => {
+    if (!props.productImage) {
+      return image.default;
+    }
+    return props.productImage;
+  }
+
+  return (
+    <React.Fragment>
+      <Col xs={12} className='product-preview-title'>Preview</Col>
+      <Col className="product px-2 " lg="3" md="4" xs="6">
+        <Row className="product-item py-2">
+          <Col className="img-col" xs="12">
+            <Image className="img" src={renderImage()} alt="Image"/>
+          </Col>
+          <Col xs="12" className='product-name py-0'>
+            <h6>{productName ? productName : ''}</h6>
+          </Col>
+          <Col className="regular-price pb-2" xs={6}>
+            <NumberFormat displayType={'text'}
+                          prefix={'Rs. '}
+                          decimalScale={2}
+                          fixedDecimalScale={true}
+                          thousandSeparator={true}
+                          value={price ? price : ''}/>
+          </Col>
+          <Col className="discount-price pb-2" xs={6}>
+            <NumberFormat displayType={'text'}
+                          prefix={'Rs. '}
+                          thousandSeparator={true}
+                          value={(discount && price) ? priceMain(price - discount) : ''}
+            />
+            {(discount && price) ? <span className="decimal-value">.{priceCents(price - discount)}</span> : ''}
+          </Col>
+
+          <Col xs={4}>
+            <input type="number" disabled={true} min={0} className="product-count w-100" defaultValue={1}/>
+          </Col>
+          <Col xs={8} className="product-btn">
+            <Button disabled={true} className="product-add-btn">
+              Add To Cart
+            </Button>
+          </Col>
+        </Row>
+      </Col>
+    </React.Fragment>
+  );
+}
+
+export default AddProductPreview;
--- src/components/adminDashboard/addProduct/index.tsx
+++ src/components/adminDashboard/addProduct/index.tsx
@@ -1,6 +1,7 @@
 import { Form, Input, Select } from "antd";
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import AddProductPreview from "./AddProductPreview";
 
 const AddProduct = () => {
   const [form] = Form.useForm();
@@ -18,16 +19,16 @@ const AddProduct = () => {
     );
   });
 
-  const checkPrice = (_: any, value: number) => {
+  const checkNumber = (_: any, value: number) => {
     if (value > 0) {
       return Promise.resolve();
     }
-    return Promise.reject(new Error('Price must be greater than zero!'));
+    return Promise.reject(new Error("Price must be greater than zero!"));
   };
 
-  const prefixSelector = (
+  const pricePrefixSelector = (
     <Form.Item name="prefix" noStyle>
-      <Select style={{ width: 70 }} defaultValue={'Rs'} >
+      <Select style={{ width: 70 }} defaultValue={"Rs"}>
         <Option value="Rs">Rs</Option>
       </Select>
     </Form.Item>
@@ -38,56 +39,99 @@ const AddProduct = () => {
       <Col xs={12} className="add-product-page-title">
         AddProduct
       </Col>
-      <Col xs={12}>
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          form={form}
-          onFinish={HandleOnSubmit}
-          className="add-product-form"
-        >
-          <Row>
-            <Col xs={12} md={6}>
-              <Form.Item
-                name="title"
-                label="Product name"
-                hasFeedback
-                rules={[
-                  { required: true, message: "Please enter product name" },
-                ]}
-              >
-                <Input placeholder="Enter product name" />
-              </Form.Item>
+      <Row>
+        <Col xs={12} lg={5} className='create-product-preview'>
+          <AddProductPreview
+            discount={10}
+            price={200.00} productName={"temp"} productImage={null}          
+          />
+        </Col>
+        <Col xs={12} lg={7}>
+          <Form
+            name="basic"
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+            form={form}
+            onFinish={HandleOnSubmit}
+            className="add-product-form"
+          >
+            <Row>
+              <Col xs={12} md={6}>
+                <Form.Item
+                  name="title"
+                  label="Product name"
+                  hasFeedback
+                  rules={[
+                    { required: true, message: "Please enter product name" },
+                  ]}
+                >
+                  <Input placeholder="Enter product name" />
+                </Form.Item>
 
-              <Form.Item
-                name="category"
-                label="Product category"
-                hasFeedback
-                rules={[
-                  { required: true, message: "Please enter product category" },
-                ]}
-              >
-                <Select placeholder="Select category">
-                  {renderCategories}
-                </Select>
-              </Form.Item>
+                <Form.Item
+                  name="category"
+                  label="Product category"
+                  hasFeedback
+                  rules={[
+                    { required: true, message: "Please enter product category" },
+                  ]}
+                >
+                  <Select placeholder="Select category">
+                    {renderCategories}
+                  </Select>
+                </Form.Item>
 
-              <Form.Item
-                name="quantity"
-                label="Product quantity"
-                hasFeedback
-                rules={[
-                  { required: true, message: "Please enter product quantity" },
-                  { validator: checkPrice},
-                ]}
-              >
-                <Input addonBefore={prefixSelector} placeholder="Enter product name" type='number' />
-              </Form.Item>
-            </Col>
-          </Row>
-        </Form>
-      </Col>
+                <Form.Item
+                  name="quantity"
+                  label="Product quantity"
+                  hasFeedback
+                  rules={[
+                    { required: true, message: "Please enter product quantity" },
+                    { validator: checkNumber },
+                  ]}
+                >
+                  <Input placeholder="Enter product name" type="number" />
+                </Form.Item>
+              </Col>
+
+              <Col xs={12} md={6}>
+                <Form.Item
+                  name="regular_price"
+                  label="Product regular price"
+                  hasFeedback
+                  rules={[
+                    { required: true, message: "Please enter regular price" },
+                    { validator: checkNumber },
+                  ]}
+                >
+                  <Input
+                    addonBefore={pricePrefixSelector}
+                    placeholder="Enter product  regular price"
+                    type="number"
+                  />
+                </Form.Item>
+
+                <Form.Item
+                  name="discount"
+                  label="Discount"
+                  hasFeedback
+                  rules={[
+                    { required: true, message: "Please enter regular price" },
+                    { validator: checkNumber },
+                  ]}
+                >
+                  <Input
+                    defaultValue={0.0}
+                    addonBefore={pricePrefixSelector}
+                    placeholder="Enter product  discount"
+                    type="number"
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
     </Row>
   );
 };
